122203aec165be7cd9994ff5d58fe6c41a451bd2
Aaron Spiteri <azzmosphere@gmail.com>
2026-01-30 20:34:20 +1100

    test pass

--- src/tst_motor_ctl_pigpiod.cpp
+++ src/tst_motor_ctl_pigpiod.cpp
@@ -133,7 +133,7 @@ class Motor {
         return CallbackReturn::SUCCESS;
     }
 
-    CallbackReturn on_shutdown() {
+    CallbackReturn on_deactivate() {
         CallbackReturn exit_res = CallbackReturn::SUCCESS;
         
         // dont exit early, attempt to set DIR_PIN to low, event if PWM fails.
@@ -251,6 +251,20 @@ class Motor {
     }
 };
 
+
+void test_motor(Motor &motor) {
+    motor.set_direction(FORWARD);
+
+    // should set motor to 65%
+    std::this_thread::sleep_for(std::chrono::microseconds(10)); 
+    if (motor.set_pwm(65) == OK) {  
+        // sleep for 3 seconds to test motor
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    }
+    motor.on_deactivate();
+}
+
+
 int main() {
 
     int pi = gpioInitialise();
@@ -259,8 +273,14 @@ int main() {
         return 1;
     }
 
-    Motor motor_a;
+    Motor motor_a, motor_b;
     if (motor_a.on_configure(PWM_A, DIR_A, pi) == CallbackReturn::FAILURE) {
+        gpioTerminate();
+        std::cout << "FAILED TO CONFIGURE!!! exiting program\n";
+        return 1;
+    }
+
+    if (motor_b.on_configure(PWM_B, DIR_B, pi) == CallbackReturn::FAILURE) {
         gpioTerminate();
         std::cout << "FAILED TO CONFIGURE!!! exiting program\n";
         return 1;
@@ -272,18 +292,39 @@ int main() {
         return 1;
     }
 
+    if (motor_b.on_activate() == CallbackReturn::FAILURE) {
+        gpioTerminate(); 
+        std::cout << "FAILED TO ACTIVATE!!! exiting program\n";
+        return 1;
+    }
+
+    std::cout << "Testing motor A\n";
+    test_motor(motor_a);
+
+    std::cout << "Testing motor B\n";
+    test_motor(motor_b);
+
+    // Test Motor A and B
+    motor_a.on_activate();
+    motor_b.on_activate();
     motor_a.set_direction(FORWARD);
-    // should set motor to 50%
+    motor_b.set_direction(FORWARD);
     std::this_thread::sleep_for(std::chrono::microseconds(10)); 
-    if (motor_a.set_pwm(65) == OK) {  
+    if (motor_a.set_pwm(65) == OK && motor_b.set_pwm(65) == OK) {  
+        // sleep for 3 seconds to test motor
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    }
+
+    // Increase speed motor A and B
+    if (motor_a.set_pwm(85) == OK && motor_b.set_pwm(85) == OK) {  
         // sleep for 3 seconds to test motor
         std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     }
 
 
-    motor_a.on_shutdown();
+    motor_a.on_deactivate();
+    motor_b.on_deactivate();
 
-    // motor_ctl.on_shutdown();
     gpioTerminate(); 
     return 0;
 }
